6d1ff1d9b4847e30e17d6345e453ce14361fbc4b
tzjeck <matejcelarc@gmail.com>
2015-02-13 23:58:19 +0100

    Renaming the function to a more logical name.

--- server/bl.js
+++ server/bl.js
@@ -1333,8 +1333,8 @@ exports.get_sensors_for_node = function (req, callback) {
 exports.get_sensor_history = function (req, callback) {
     db.get_sensor_history(req.data.node, req.data.sensor, callback)
 };
-exports.get_all_sensor_history = function (callback) {
-    db.get_all_sensor_history(callback)
+exports.get_all_measurements = function (callback) {
+    db.get_all_measurements(callback)
 };
 exports.get_sensor_measurement = function (req, callback) {
     db.get_sensor_measurement(req, callback)
