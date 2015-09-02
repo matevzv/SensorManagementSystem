d840f375a62ef67caf0395dadb82785247b7d3a5
Matevz Vucnik <vucnikm@gmail.com>
2015-09-02 11:46:20 +0200

    remove unused certificates

--- server/config/passport.js
+++ server/config/passport.js
@@ -15,8 +15,6 @@ passport.use('config1', new SamlStrategy(
     entryPoint: 'https://ds-edugain.aai.arnes.si/simplesaml/saml2/sp/idpdisco.php',
     issuer: 'https://www.e-osu.si/metadata.xml',
     callbackUrl: '/loginaai',
-    cert: fs.readFileSync('./cert/cert.pem', 'utf-8'),
-    privateCert: fs.readFileSync('./cert/key.pem', 'utf-8'),
     additionalParams:{'entityID':'https://www.e-osu.si/metadata.xml','returnIDParam':'entityID','return':'https://www.e-osu.si/loginaai'}
   },
   function(profile, done) {
@@ -26,26 +24,23 @@ passport.use('config1', new SamlStrategy(
         email : profile.mail
       });
     }
-));
+  ));
 
 passport.use('config2', new SamlStrategy(
   {
     entryPoint: 'https://idp.aai.arnes.si/simplesaml/saml2/idp/SSOService.php',
     issuer: 'https://www.e-osu.si/metadata.xml',
     callbackUrl: '/assert',
-    //cert: fs.readFileSync('./cert/cert.pem', 'utf-8'),
-    //privateCert: fs.readFileSync('./cert/key.pem', 'utf-8'),
     additionalParams:{'RelayState':'cookie'}
   },
   function(profile, done) {
     return done(null,
       {
         userName: profile.eduPersonPrincipalName,
-        email: profile.mail[0],
         lastName:  profile.sn,
         firstName: profile.givenName
       });
     }
-));
+  ));
 
 module.exports = passport;
