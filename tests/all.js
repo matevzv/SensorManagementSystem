df04d121eeb0b95251a3ece8269e8077fe2447d0
Matevz Vucnik <vucnikm@gmail.com>
2016-04-22 15:58:08 +0200

    huge refactoring

---
+++ tests/all.js
@@ -0,0 +1,26 @@
+
+exports['test basic operation'] = function(assert, done) {
+	var fs = require('fs')
+	var http = require('http')
+
+	var login_file = fs.readFileSync('public/login.html','utf8')
+
+	var options = {
+		host: 'localhost',
+		path: '/login'
+	}
+	var request = http.get(options, function (response) {
+		response.setEncoding('utf8')
+		var str = '';
+		response.on('data', function (chunk) {
+			str += chunk;
+		})
+
+		response.on('end', function () {
+			assert.equal(str, login_file, 'Login page loaded successfully!')
+			done()
+		})
+	})
+}
+
+if (module == require.main) require('test').run(exports)
