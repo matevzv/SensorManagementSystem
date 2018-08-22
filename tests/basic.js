ace66933c1e4290b9470803538a19e4ceaae4203
Matevz Vucnik <vucnikm@gmail.com>
2018-08-22 15:53:40 +0200

    fix

--- tests/basic.js
+++ tests/basic.js
@@ -5,7 +5,7 @@ chai.use(chaiHttp);
 
 describe('test basic operation', function() {
   it('should run server and load login page', function(done) {
-    this.timeout(120000);
+    this.timeout(60000);
     var spawn = require('child_process').spawn;
     var srv = spawn('node', ['app.js']);
 
